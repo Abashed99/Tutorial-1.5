fd716d694f99fb48ee2d9c60a0909c1b9ca0da15
Abashed99 <122331924+Abashed99@users.noreply.github.com>
2023-02-28 04:29:49 -0500

    Add Animation

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -3,54 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class audio : MonoBehaviour
+public class CatController : MonoBehaviour
 {
     public AudioSource musicSource;
     public AudioClip musicClipOne;
 
     public AudioClip musicClipTwo;
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {    
+     anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+         if (Input.GetKeyDown(KeyCode.W))
+
         {
+
           musicSource.clip = musicClipOne;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.R))
+
         {
+
           musicSource.clip = musicClipTwo;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
          }
 
      if (Input.GetKeyUp(KeyCode.R))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.L))
+
         {
+
           musicSource.loop = true;
+
          }
 
      if (Input.GetKeyUp(KeyCode.L))
+
         {
+
           musicSource.loop = false;
+
         }
     }
 }
+
